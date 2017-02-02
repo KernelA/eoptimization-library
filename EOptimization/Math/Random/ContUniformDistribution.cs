ee956e3fbc0ad66716201c3e48ba4e6e1c89a783
KernelA <alex9x99@gmail.com>
2017-02-02 13:35:52 +0300

    Add random geneartor with normal and  unifrom distribution.

---
+++ EOptimization/Math/Random/ContUniformDistribution.cs
@@ -0,0 +1,100 @@
+﻿namespace EOpt.Math.Random
+{
+    using System;
+
+    /// <summary>
+    /// Continuous uniform distribution.
+    /// </summary>
+    public class ContUniformDistribution : IContUniformGenerator
+    {
+
+        private double a, b;
+
+        private Random rand;
+
+        /// <summary>
+        /// The left boundary of the distribution range.
+        /// </summary>
+        public double LeftBound
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if (value > b)
+                    throw new ArgumentException($"{LeftBound} must be less than right boundary.", nameof(LeftBound));
+
+                a = value;
+            }
+        }
+
+        /// <summary>
+        /// The right boundary of the distribution range.
+        /// </summary>
+        public double RightBound
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value < a)
+                    throw new ArgumentException($"{RightBound} must be greater than left boundary.", nameof(RightBound));
+
+                b = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Create continuous uniform distribution on [0; 1].
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        public ContUniformDistribution() : this(0, 1)
+        {
+        }
+
+        /// <summary>
+        /// Create continuous uniform distribution on [<paramref name="LeftBound"/>; <paramref name="RightBound"/>].
+        /// </summary>
+        /// <param name="LeftBound">The left boundary of the distribution range.</param>
+        /// <param name="RightBound">The right boundary of the distribution range.</param>
+        /// <exception cref="ArgumentException"></exception>
+        public ContUniformDistribution(double LeftBound, double RightBound)
+        {
+            if (LeftBound > RightBound)
+                throw new ArgumentException($"{RightBound} must be greater than {LeftBound}.");
+
+            this.LeftBound = LeftBound;
+            this.RightBound = RightBound;
+
+            rand = SyncRandom.Get();
+        }
+
+        /// <summary>
+        /// Random value from continuous uniform distribution, where distribution range specified in class.
+        /// </summary>
+        /// <returns></returns>
+        public double RandVal()
+        {
+            return URandVal(LeftBound, RightBound);
+        }
+
+        /// <summary>
+        /// Random value from continuous uniform distribution on [<paramref name="LeftBound"/>; <paramref name="RightBound"/>].
+        /// </summary>
+        /// <param name="LeftBound">The left boundary of the distribution range.</param>
+        /// <param name="RightBound">The right boundary of the distribution range.</param>
+        /// <returns></returns>
+        public double URandVal(double LeftBound, double RightBound)
+        {
+            if (LeftBound > RightBound)
+                throw new ArgumentException($"{RightBound} must be greater than {LeftBound}.");
+
+            return (RightBound - LeftBound) * rand.NextDouble() + LeftBound;
+        }
+    }
+}
---
+++ EOptimization/Math/Random/NormalDistribution.cs
@@ -0,0 +1,116 @@
+﻿namespace EOpt.Math.Random
+{
+    using System;
+
+    /// <summary>
+    /// Normal distribution.
+    /// </summary>
+    public class NormalDistribution : INormalGenerator
+    {
+
+        private double mean, stdDev;
+
+        private Random rand;
+
+        /// <summary>
+        /// Mean value.
+        /// </summary>
+        public double Mean
+        {
+            get
+            {
+                return mean;
+            }
+            set
+            {
+                mean = value;
+            }
+        }
+
+        /// <summary>
+        /// Standard deviation.
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        public double StdDev
+        {
+            get
+            {
+                return stdDev;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException($"{nameof(StdDev)} must be > 0.", nameof(StdDev));
+
+                stdDev = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Create normal distribution with mean  equal 0 and standard deviation equal 1.
+        /// </summary>
+        public NormalDistribution() : this(0, 1)
+        {
+
+        }
+
+        /// <summary>
+        /// Create normal distribution with mean  equal <paramref name="Mean"/> and standard deviation equal <paramref name="StdDev"/>.
+        /// </summary>
+        /// <param name="Mean">Mean value.</param>
+        /// <param name="StdDev">Standard deviation.</param>
+        /// <exception cref="ArgumentException"></exception>
+        public NormalDistribution(double Mean, double StdDev)
+        {
+            rand = SyncRandom.Get();
+
+            this.Mean = Mean;
+            this.StdDev = StdDev;
+
+        }
+
+        /// <summary>
+        /// Random value from normal distribution with mean equal <paramref name="Mean"/> and standard deviation equal <paramref name="StdDev"/>.
+        /// </summary>
+        /// <remarks>Using Box–Muller transform.</remarks>
+        /// <param name="Mean">Mean.</param>
+        /// <param name="StdDev">Standard deviation.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public double NRandVal(double Mean, double StdDev)
+        {
+            if (StdDev <= 0)
+                throw new ArgumentException($"{nameof(StdDev)} must be > 0.", nameof(StdDev));
+
+            double u = 0.0;
+            double v = 0.0;
+
+            double s = 0.0;
+
+            do
+            {
+                u = 2.0 * rand.NextDouble() - 1.0;
+                v = 2.0 * rand.NextDouble() - 1.0;
+                s = u * u + v * v;
+            }
+            while (s > 1.0 || s < 1E-10);
+
+            return Mean + StdDev * (u * Math.Sqrt(-2 * Math.Log(s) / s));
+        }
+
+
+        /// <summary>
+        /// Random value from normal distribution where mean and standard deviation specified in class.
+        /// </summary>
+        /// <remarks>Using Box–Muller transform.</remarks>
+        /// <param name="Mean">Mean.</param>
+        /// <param name="StdDev">Standard deviation.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public double RandVal()
+        {
+            return NRandVal(Mean, StdDev);
+        }
+    }
+}
