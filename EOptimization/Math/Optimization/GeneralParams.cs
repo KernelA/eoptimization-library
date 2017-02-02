647c5b8b3c64180b8562e2e07bde3f9fab438d9d
KernelA <alex9x99@gmail.com>
2017-02-02 13:39:00 +0300

    Add general interface for optimization methods.

---
+++ EOptimization/Math/Optimization/GeneralParams.cs
@@ -0,0 +1,57 @@
+﻿namespace EOpt.Math.Optimization
+{
+
+    using System;
+
+    /// <summary>
+    /// General parameters for  <see cref="IOptimizer.Optimize(GeneralParams)"/>. See interface <see cref="IOptimizer"/>.
+    /// </summary>
+    public class GeneralParams
+    {
+
+        public double[] LeftBound { get; private set; }
+
+        public double[] RightBound { get; private set; }
+
+        /// <summary>
+        /// Target function.
+        /// </summary>
+        public Func<double[], double> ObjectiveFunction { get; private set; }
+
+        /// <summary>
+        /// <para>
+        /// Parameters for <see cref="IOptimizer.InitializeParameters(object)"/>.
+        /// </para>
+        /// <para>
+        /// Constraints is rectangular parallelepiped. First vertex of rectangular parallelepiped has coordinates <paramref name="leftBound[i]"/>, where i from 1 to dimension of space.
+        /// Second vertex of rectangular parallelepiped has coordinates <paramref name="rightBound[i]"/>, where i from 1 to dimension of space.
+        /// </para>
+        /// </summary>
+        /// <param name="objFunction">Target function.</param>
+        /// <param name="leftBound">Coordinates of first vertex.</param>
+        /// <param name="rightBound">Coordinates of second vertex</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        public GeneralParams(Func<double[], double> objFunction, double[] leftBound, double[] rightBound)
+        {
+            if (leftBound == null)
+                throw new ArgumentNullException(nameof(leftBound));
+            if (rightBound == null)
+                throw new ArgumentNullException(nameof(rightBound));
+            if (objFunction == null)
+                throw new ArgumentNullException(nameof(objFunction));
+            if (leftBound.Length != rightBound.Length)
+                throw new ArgumentException("Length " + nameof(leftBound) + " and " + nameof(rightBound) + " must be equal.");
+
+            for (int i = 0; i < leftBound.Length; i++)
+            {
+                if (leftBound[i] > rightBound[i])
+                    throw new ArgumentException(nameof(leftBound) + $"[{i}]" + " greater than " + nameof(rightBound) + $"[{i}].");
+            }
+
+            LeftBound = leftBound;
+            RightBound = rightBound;
+            ObjectiveFunction = objFunction;
+        }
+    }
+}
---
+++ EOptimization/Math/Optimization/IOptimizer.cs
@@ -0,0 +1,30 @@
+﻿namespace EOpt.Math.Optimization
+{
+    using System;
+
+    /// <summary>
+    /// Interface for optimization methods.
+    /// </summary>
+    public interface IOptimizer
+    {
+        /// <summary>
+        /// The solution of the constrained optimization problem.
+        /// </summary>
+        PointND Solution { get; }
+
+        /// <summary>
+        /// Initializing parameters of methods.
+        /// </summary>
+        /// <param name="parametrs"></param>
+        void InitializeParameters(object parametrs);
+
+        /// <summary>
+        /// Find solution of the constrained optimization problem. If you want see progress, then you need set <paramref name="reporter"/>.
+        /// </summary>
+        /// <param name="parametrs">General parameters <see cref="GeneralParams"/>.</param>
+        /// <param name="reporter">Object which implement interface <see cref="IProgress{Tuple{int,int,int}}"/>, where first item in tuple is the initial value, second item is the end value, third item is the current progress value.</param>
+        void Optimize(GeneralParams parametrs,  IProgress<Tuple<int,int,int>> reporter = null);
+    }
+
+    
+}
