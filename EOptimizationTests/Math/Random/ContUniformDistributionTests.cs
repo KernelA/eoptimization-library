aaab9c27f9514ecf48d35e7759a6939753c9f398
KernelA <alex9x99@gmail.com>
2017-02-02 13:45:19 +0300

    Add unit tests for normal and unifrom distribution.

---
+++ EOptimizationTests/Math/Random/ContUniformDistributionTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EOpt.Math.Random;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOpt.Math.Random.Tests
+{
+    [TestClass()]
+    public class ContUniformDistributionTests
+    {
+        [TestMethod()]
+        public void ContUniformDistributionConstrTest()
+        {
+            ContUniformDistribution dist = new ContUniformDistribution();
+
+            Assert.IsTrue(dist.LeftBound == 0 && dist.RightBound == 1);
+        }
+
+        [TestMethod()]
+        public void ContUniformDistributionConstrTest1()
+        {
+            bool error = true;
+
+            try
+            {
+                ContUniformDistribution dist = new ContUniformDistribution(-2, -3);
+            }
+            catch(ArgumentException exc)
+            {
+                error = false;
+            }
+
+            Assert.IsFalse(error);
+        }
+
+
+        [TestMethod()]
+        public void URandValTest()
+        {
+            bool error = true;
+
+            ContUniformDistribution dist = new ContUniformDistribution();
+
+            try
+            {
+                dist.URandVal(3, 2);
+            }
+            catch (ArgumentException exc)
+            {
+                error = false;
+            }
+
+            Assert.IsFalse(error);
+        }
+    }
+}
---
+++ EOptimizationTests/Math/Random/NormalDistributionTests.cs
@@ -0,0 +1,70 @@
+﻿namespace EOpt.Math.Random.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using EOpt.Math.Random;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    [TestClass()]
+    public class NormalDistributionTests
+    {
+        [TestMethod()]
+        public void NormalDistributionConstrTest()
+        {
+            NormalDistribution dist = new NormalDistribution();
+
+            Assert.IsTrue(dist.Mean == 0 && dist.StdDev == 1);
+        }
+
+        [TestMethod()]
+        public void NormalDistributionConstrTest1()
+        {
+            bool error = true;
+
+            try
+            {
+                NormalDistribution dist = new NormalDistribution(1, -2);
+            }
+            catch(ArgumentException exc)
+            {
+                error = false;
+            }
+
+            Assert.IsFalse(error);
+            
+        }
+
+        [TestMethod()]
+        public void NormalDistributionConstrTest3()
+        {
+            NormalDistribution dist = new NormalDistribution(2, 5);
+
+            Assert.IsTrue(dist.Mean == 2 && dist.StdDev == 5);
+        }
+
+        [TestMethod()]
+        public void NRandValTest()
+        {
+            NormalDistribution dist = new NormalDistribution(0, 2);
+
+            bool error = true;
+
+            try
+            {
+                dist.NRandVal(1, -1);
+            }
+            catch(ArgumentException exc)
+            {
+                error = false;
+            }
+
+            Assert.IsFalse(error);
+        }
+
+   
+    }
+}
