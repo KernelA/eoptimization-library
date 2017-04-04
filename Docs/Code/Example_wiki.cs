ebee2a6f0810557376ba4df36c83f7380634af0c
KernelA <alex9x99@gmail.com>
2017-04-04 20:28:35 +0300

    Chaned source code for example.

--- Docs/Code/Example_wiki.cs
+++ Docs/Code/Example_wiki.cs
@@ -10,54 +10,71 @@ namespace Test
     {
         static void Main(string[] args)
         {
-
+            /// See http://www.sfu.ca/~ssurjano/rastr.html
             Func<double[], double> func = (x) => 20 + (x[0] * x[0] - 10 * Math.Cos(2 * Math.PI * x[0])) +
                (x[1] * x[1] - 10 * Math.Cos(2 * Math.PI * x[1]));
 
 
-            IOptimizer[] opts = {
-                 new BBBCOptimizer(),
-                 new FireworksOptimizer(),
-                 new GEMOptimizer()
-            };
+            IOptimizer<BBBCParams> bbbc = new BBBCOptimizer();
+            IOptimizer<FWParams> fw = new FWOptimizer();
+            IOptimizer<GEMParams> gem = new GEMOptimizer();
 
             // Distance between points need for Fireworks method.
             // It is squared Euclidean distance.
             Func<PointND, PointND, double> distance = (a, b) => (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]);
 
 
-            object[] parameters =  {
-                new BBBCParams(20, 100, 0.4, 0.5),
-                new FireWorksParams(20, 50, distance, 20),
-                new GEMParams(1, 20, 50, 2 * Math.Sqrt(2), 100)
-             };
-         
+            BBBCParams param1 = new BBBCParams(20, 100, 0.4, 0.5);
+            FWParams param2 = new FWParams(20, 100, distance, 20);
+            GEMParams param3 = new GEMParams(1, 100, 50, 2 * Math.Sqrt(2), 100);
+
+
             double[] constr1 = { -5.12, -5.12 };
             double[] constr2 = { 5.12, 5.12 };
 
             GeneralParams param = new GeneralParams(func, constr1, constr2);
 
-            string[] names =
-            {
-                "BBBC",
-                "Fireworks",
-                "GEM"
-            };
 
+            Console.WriteLine("Exact solution: f(x) = 0, x = (0,0).");
+            Console.WriteLine();
 
-            for (int i = 0; i < opts.Length; i++)
-            {
-                opts[i].InitializeParameters(parameters[i]);
+            Test(bbbc, param1, param, "BBBC");
+            Test(fw, param2, param, "Fireworks");
+            Test(gem, param3, param, "GEM");
 
-                opts[i].Optimize(param);
-
-                Console.WriteLine($"Method: {names[i]}.");
-                Console.WriteLine(opts[i].Solution);
-                Console.WriteLine();
-            }
-            
             Console.WriteLine("Complete");
             Console.ReadKey();
+        }
+
+        static void Test<T>(IOptimizer<T> Opt, T Parameters, GeneralParams GenParam, string Method)
+        {
+            Opt.InitializeParameters(Parameters);
+
+            PointND bestSolution = null;
+
+            double min = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Opt.Optimize(GenParam);
+
+                if (bestSolution == null)
+                {
+                    bestSolution = Opt.Solution;
+                    min = bestSolution[2];
+                }
+                    
+                else if(Opt.Solution[2] < min)
+                {
+                    bestSolution = Opt.Solution;
+                    min = bestSolution[2];
+                }
+            }
+            
+
+            Console.WriteLine($"Method: {Method}.");
+            Console.WriteLine($"Solution: f(x) = {bestSolution[2]}, x = ({bestSolution[0]}, {bestSolution[1]}).");
+            Console.WriteLine();
         }
     }
 }
